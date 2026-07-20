6b492f375411a9c96a6763371bfbac315c1fc4ec
kyoheiehime089-cpu <kyohei.ehime089@gmail.com>
2026-07-20 17:25:20 +0900

    Add PWA service worker

---
+++ gyotoku-gym24-booking/sw.js
@@ -0,0 +1,5 @@
+const CACHE='gyotoku-gym24-pwa-v1';
+const ASSETS=['./','./index.html','./admin.html','./member-style.css?v=43','./admin-style.css?v=40','./icon-member.svg','./icon-admin.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin!==self.location.origin)return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});return response;}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./'))));});
